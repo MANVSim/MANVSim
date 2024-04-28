1be53f999254590e484d8a513a181317ff20ac21
Jon <jonstuehrwoldt@gmx.de>
2024-04-29 01:52:19 +0200

    app #7 rebase on main

---
+++ app/lib/appframe.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:manvsim/patientlist.dart';
+
+class AppFrame extends StatefulWidget {
+  const AppFrame({super.key});
+
+  @override
+  State<AppFrame> createState() => _AppFrameState();
+}
+
+class _AppFrameState extends State<AppFrame> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = Placeholder();
+        break;
+      case 1:
+        page = PatientListPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: const [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.list),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+                setState(() {
+                  selectedIndex = value;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: page,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
