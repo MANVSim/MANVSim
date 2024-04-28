6ebad3b8c1bd05c3d600471df477eee602cd7833
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2024-04-28 16:34:54 +0200

    refactor: Normalize schema tables and insert initial data
    - Refactored the schema tables to remove unnecessary double quotes and
      maintain a consistent naming convention.
    - Inserted initial data into the 'scenario', 'execution', and 'player' tables to provide a baseline for testing and development.

--- assets/MANV scheme.sql
+++ assets/MANV scheme.sql
@@ -1,58 +1,69 @@
-CREATE TABLE IF NOT EXISTS "scenario" (
-	"id" INTEGER NOT NULL UNIQUE,
-	"name" TEXT,
-	PRIMARY KEY("id")	
+CREATE TABLE IF NOT EXISTS scenario (
+	id INTEGER NOT NULL UNIQUE,
+	name TEXT,
+	PRIMARY KEY(id)	
 );
 
-CREATE TABLE IF NOT EXISTS "execution" (
-	"tan" TEXT NOT NULL UNIQUE,
-	"scenario_id" INTEGER,
-	"start" TEXT,
-	PRIMARY KEY("tan"),
-	FOREIGN KEY ("scenario_id") REFERENCES "scenario"("id")
-	ON UPDATE NO ACTION ON DELETE NO ACTION
+INSERT INTO scenario ( 
+    id", "name 
+) VALUES 
+( 0, "Bus Crash" ),
+( 1, "Train Crash" );
+
+CREATE TABLE IF NOT EXISTS execution (
+	tan TEXT NOT NULL UNIQUE,
+	scenario_id INTEGER NOT NULL REFERENCES scenario(id),
+	start TEXT,
+	PRIMARY KEY(tan),
 );
 
-CREATE TABLE IF NOT EXISTS "player" (
-	"tan" TEXT NOT NULL UNIQUE,
-	"execution_id" TEXT,
-	PRIMARY KEY("tan"),
-	FOREIGN KEY ("execution_id") REFERENCES "execution"("tan")
-	ON UPDATE NO ACTION ON DELETE NO ACTION
+INSERT INTO execution (
+    tan, scenario_id, start
+) VALUES 
+( "XYZ123", 0, "1714314018" )
+( "ABC456", 1, "1719577218" );
+
+CREATE TABLE IF NOT EXISTS player (
+	tan TEXT NOT NULL UNIQUE,
+	execution_id TEXT NOT NULL REFERENCES execution(tan),
+	PRIMARY KEY(tan),
 );
 
-CREATE TABLE IF NOT EXISTS "patients" (
-	"id" INTEGER NOT NULL UNIQUE,
-	"injuries" BLOB,
-	"activity_diagram" BLOB,
-	PRIMARY KEY("id")	
+INSERT INTO player (
+    tan, execution_id
+) VALUES 
+( "QWERTZ", "XYZ123" ),
+( "WASD42", "XYZ123" ),
+( "1337OP", "ABC456" );
+
+CREATE TABLE IF NOT EXISTS patients (
+	id INTEGER NOT NULL UNIQUE,
+	injuries BLOB,
+	activity_diagram BLOB,
+	PRIMARY KEY(id)	
 );
 
-CREATE TABLE IF NOT EXISTS "takes_part_in" (
-	"id" INTEGER NOT NULL UNIQUE,
-	"scenario_id" INTEGER,
-	"patient_id" INTEGER,
-	PRIMARY KEY("id"),
-	FOREIGN KEY ("scenario_id") REFERENCES "scenario"("id")
-	ON UPDATE NO ACTION ON DELETE NO ACTION
+CREATE TABLE IF NOT EXISTS takes_part_in (
+	id INTEGER NOT NULL UNIQUE,
+	scenario_id INTEGER NOT NULL REFERENCES scenario(id),
+	patient_id INTEGER NOT NULL REFERENCES patients(id),
+	PRIMARY KEY(id),
 );
 
-CREATE TABLE IF NOT EXISTS "action" (
-	"id" INTEGER NOT NULL UNIQUE,
-	"name" TEXT,
-	"picture" BLOB,
-	"results" BLOB,
-	"time" TEXT,
-	PRIMARY KEY("id")	
+CREATE TABLE IF NOT EXISTS action (
+	id INTEGER NOT NULL UNIQUE,
+	name TEXT,
+	picture BLOB,
+	results BLOB,
+	time TEXT,
+	PRIMARY KEY(id)	
 );
 
-CREATE TABLE IF NOT EXISTS "performed_actions" (
-	"id" INTEGER NOT NULL UNIQUE,
-	"time" TEXT,
-	"execution_id" TEXT,
-	"patient_id" INTEGER,
-	"action_id" INTEGER,
-	PRIMARY KEY("id"),
-	FOREIGN KEY ("execution_id") REFERENCES "execution"("tan")
-	ON UPDATE NO ACTION ON DELETE NO ACTION
+CREATE TABLE IF NOT EXISTS performed_actions (
+	id INTEGER NOT NULL UNIQUE,
+	time TEXT,
+	execution_id TEXT NOT NULL REFERENCES execution(tan),
+	patient_id INTEGER NOT NULL REFERENCES patients(id),
+	action_id INTEGER NOT NULL REFERENCES action(id),
+	PRIMARY KEY(id),
 );
