d12a7f26ff2fdded4e8c66b5654d78205aa12b45
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2024-05-02 17:30:36 +0200

    refactor: add django auth tables to scheme

--- doc/server/assets/MANV scheme.sql
+++ doc/server/assets/MANV scheme.sql
@@ -1,69 +1,88 @@
-CREATE TABLE IF NOT EXISTS scenario (
-	id INTEGER NOT NULL UNIQUE,
-	name TEXT,
-	PRIMARY KEY(id)	
+CREATE TABLE IF NOT EXISTS "scenario" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"name" TEXT,
+	PRIMARY KEY("id")	
 );
 
-INSERT INTO scenario ( 
-    id", "name 
-) VALUES 
-( 0, "Bus Crash" ),
-( 1, "Train Crash" );
-
-CREATE TABLE IF NOT EXISTS execution (
-	tan TEXT NOT NULL UNIQUE,
-	scenario_id INTEGER NOT NULL REFERENCES scenario(id),
-	start TEXT,
-	PRIMARY KEY(tan),
+CREATE TABLE IF NOT EXISTS "execution" (
+	"tan" TEXT NOT NULL UNIQUE,
+	"scenario_id" INTEGER,
+	"start" TEXT,
+	PRIMARY KEY("tan"),
+	FOREIGN KEY ("scenario_id") REFERENCES "scenario"("id")
+	ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
-INSERT INTO execution (
-    tan, scenario_id, start
-) VALUES 
-( "XYZ123", 0, "1714314018" )
-( "ABC456", 1, "1719577218" );
-
-CREATE TABLE IF NOT EXISTS player (
-	tan TEXT NOT NULL UNIQUE,
-	execution_id TEXT NOT NULL REFERENCES execution(tan),
-	PRIMARY KEY(tan),
+CREATE TABLE IF NOT EXISTS "player" (
+	"tan" TEXT NOT NULL UNIQUE,
+	"execution_id" TEXT,
+	PRIMARY KEY("tan"),
+	FOREIGN KEY ("execution_id") REFERENCES "execution"("tan")
+	ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
-INSERT INTO player (
-    tan, execution_id
-) VALUES 
-( "QWERTZ", "XYZ123" ),
-( "WASD42", "XYZ123" ),
-( "1337OP", "ABC456" );
-
-CREATE TABLE IF NOT EXISTS patients (
-	id INTEGER NOT NULL UNIQUE,
-	injuries BLOB,
-	activity_diagram BLOB,
-	PRIMARY KEY(id)	
+CREATE TABLE IF NOT EXISTS "patients" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"injuries" BLOB,
+	"activity_diagram" BLOB,
+	PRIMARY KEY("id")	
 );
 
-CREATE TABLE IF NOT EXISTS takes_part_in (
-	id INTEGER NOT NULL UNIQUE,
-	scenario_id INTEGER NOT NULL REFERENCES scenario(id),
-	patient_id INTEGER NOT NULL REFERENCES patients(id),
-	PRIMARY KEY(id),
+CREATE TABLE IF NOT EXISTS "takes_part_in" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"scenario_id" INTEGER,
+	"patient_id" INTEGER,
+	PRIMARY KEY("id"),
+	FOREIGN KEY ("scenario_id") REFERENCES "scenario"("id")
+	ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
-CREATE TABLE IF NOT EXISTS action (
-	id INTEGER NOT NULL UNIQUE,
-	name TEXT,
-	picture BLOB,
-	results BLOB,
-	time TEXT,
-	PRIMARY KEY(id)	
+CREATE TABLE IF NOT EXISTS "action" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"name" TEXT,
+	"picture" BLOB,
+	"results" BLOB,
+	"time" TEXT,
+	PRIMARY KEY("id")	
 );
 
-CREATE TABLE IF NOT EXISTS performed_actions (
-	id INTEGER NOT NULL UNIQUE,
-	time TEXT,
-	execution_id TEXT NOT NULL REFERENCES execution(tan),
-	patient_id INTEGER NOT NULL REFERENCES patients(id),
-	action_id INTEGER NOT NULL REFERENCES action(id),
-	PRIMARY KEY(id),
+CREATE TABLE IF NOT EXISTS "performed_actions" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"time" TEXT,
+	"execution_id" TEXT,
+	"patient_id" INTEGER,
+	"action_id" INTEGER,
+	PRIMARY KEY("id"),
+	FOREIGN KEY ("execution_id") REFERENCES "execution"("tan")
+	ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE TABLE IF NOT EXISTS "auth_user" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"username" TEXT NOT NULL UNIQUE,
+	"password" TEXT NOT NULL,
+	"email" TEXT NOT NULL,
+	"is_superuser" INTEGER NOT NULL DEFAULT false,
+	"first_name" TEXT NOT NULL,
+	"last_name" TEXT NOT NULL,
+	"is_staff" INTEGER NOT NULL DEFAULT false,
+	"is_active" INTEGER NOT NULL DEFAULT true,
+	"date_joined" TEXT NOT NULL,
+	"last_login" TEXT,
+	PRIMARY KEY("id")	
+);
+
+CREATE TABLE IF NOT EXISTS "auth_group" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"name" TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id")	
+);
+
+CREATE TABLE IF NOT EXISTS "auth_user_groups" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"user_id" INTEGER NOT NULL,
+	"auth_group" INTEGER NOT NULL,
+	PRIMARY KEY("id"),
+	FOREIGN KEY ("user_id") REFERENCES "auth_user"("id")
+	ON UPDATE NO ACTION ON DELETE NO ACTION
 );
