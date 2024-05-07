f1295ec14006253129a33284bc60f7ffa30f499a
Simon Ohlsen <stu216738@mail.uni-kiel.de>
2024-05-07 14:39:04 +0200

    add missing attributes, remove .json, add graph

--- server/entities/patient.py
+++ server/entities/patient.py
@@ -3,9 +3,10 @@ from location import Location
 
 class Patient:
 
-    def __init__(self, id: int, injuries: str, activity_diagram: str,
+    def __init__(self, id: int, name: str, injuries: str, activity_diagram: str,
                  location: Location):
         self.id = id
+        self.name = name
         self.injuries = injuries  # FIXME: Maybe replace by JSON datatype
         self.activity_diagram = activity_diagram  # FIXME: Maybe replace JSON datatype
         self.location = location
--- server/entities/resource_type.py
+++ server/entities/resource_type.py
@@ -1,6 +1,6 @@
 class ResourceType:
 
-    def __init__(self, id: int, name: str, picture: str):
+    def __init__(self, id: int, name: str, picture_ref: str):
         self.id = id
         self.name = name
-        self.picture = picture  # Reference to picture
+        self.picture_ref = picture_ref  # Reference to picture
