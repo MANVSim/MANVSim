a701a1b697b6f6ed43ef590d2e6674be35932c48
Simon Ohlsen <stu216738@mail.uni-kiel.de>
2024-05-08 11:18:06 +0200

    add performed action class, update graph, add execution status

--- server/entities/execution.py
+++ server/entities/execution.py
@@ -1,11 +1,21 @@
+from enum import Enum
+
 from player import Player
 from scenario import Scenario
 
 
 class Execution:
 
-    def __init__(self, id: int, scenario: Scenario, starting_time: int, players: list[Player]):
+    class Status(Enum):
+        RUNNING = "running"
+        WAITING = "pending"
+        STOPPED = "finished"
+        UNKNOWN = "unknown"
+
+    def __init__(self, id: int, scenario: Scenario, starting_time: int, players: list[Player], status: Status):
         self.id = id  # FIXME: Maybe replace by TAN
         self.scenario = scenario
         self.starting_time = starting_time  # FIXME: Maybe replace by standardized time format
         self.players = players
+        self.status = status
+
---
+++ server/entities/performed_action.py
@@ -0,0 +1,17 @@
+from action import Action
+from patient import Patient
+from player import Player
+from resource import Resource
+
+
+class PerformedAction:
+
+    def __init__(self, id: int, time: int, execution: str, patient: Patient, action: Action,
+                 resources_used: list[Resource], player: Player):
+        self.id = id
+        self.time = time  # FIXME: Maybe replace by standardized time format
+        self.execution = execution  # TAN
+        self.patient = patient
+        self.action = action
+        self.resources_used = resources_used
+        self.player = player
