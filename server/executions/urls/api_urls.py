c074961727c3c3d92c126ed429eed27241f0eb1c
stu216909 <stu216909@mail.uni-kiel.de>
2024-05-08 12:14:55 +0200

    add csrf + register entry (draft)

--- server/executions/urls/api_urls.py
+++ server/executions/urls/api_urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from ..views import register
+from ..views import register, security
 
 urlpatterns = [
-    path('register/hello/', register.hello_world)
+    path('register/hello/', register.hello_world),
+    path('register/', register.register_player),
+
+    path('security/csrf/', security.csrf)
 ]
--- server/executions/views/register.py
+++ server/executions/views/register.py
@@ -1,7 +1,30 @@
-from django.http import JsonResponse
-from django.shortcuts import render
+import logging
+
+from django.http import JsonResponse, HttpRequest, HttpResponse
+from http import HTTPStatus
+
+from django.utils.datastructures import MultiValueDictKeyError
 
 
 def hello_world(request):
     data = {"hello": "world"}
     return JsonResponse(data)
+
+
+def register_player(request: HttpRequest):
+    response = HttpResponse()
+    if request.method != "POST":
+        response.status_code = HTTPStatus.BAD_REQUEST
+        return response
+
+    try:
+        data = request.POST
+        tan = data["TAN"]
+        logging.debug(f"Player registered with data: {tan}")
+        # TODO
+        response.status_code = HTTPStatus.OK
+        return response
+
+    except MultiValueDictKeyError:
+        response.status_code = HTTPStatus.BAD_REQUEST
+        return response
