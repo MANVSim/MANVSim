9eed62234b369208c8bb91cd79c39b83f686e063
Simon Ohlsen <stu216738@mail.uni-kiel.de>
2024-05-09 13:52:03 +0200

    add to_dict and to_json methods, update attributes

--- server/executions/entities/resource.py
+++ server/executions/entities/resource.py
@@ -1,5 +1,7 @@
-from location import Location
-from resource_type import ResourceType
+import json
+
+from server.executions.entities.location import Location
+from server.executions.entities.resource_type import ResourceType
 
 
 class Resource:
@@ -9,3 +11,22 @@ class Resource:
         self.location = location
         self.resource_type = resource_type
         self.quantity = quantity
+
+    def to_dict(self, shallow: bool = False):
+        """
+        Returns all fields of this class in a dictionary. By default, all nested objects are included. In case the
+        'shallow'-flag is set, only the object reference in form of a unique identifier is included.
+        """
+        return {
+            'id': self.id,
+            'location': self.location.id if shallow else self.location.to_dict(),
+            'resource_type': self.resource_type.id if shallow else self.resource_type.to_dict(),
+            'quantity': self.quantity
+        }
+
+    def to_json(self, shallow: bool = False):
+        """
+        Returns this object as a JSON. By default, all nested objects are included. In case the 'shallow'-flag is set,
+        only the object reference in form of a unique identifier is included.
+        """
+        return json.dumps(self.to_dict(shallow))
--- server/executions/entities/resource_type.py
+++ server/executions/entities/resource_type.py
@@ -1,6 +1,15 @@
+import json
+
+
 class ResourceType:
 
     def __init__(self, id: int, name: str, picture_ref: str):
         self.id = id
         self.name = name
         self.picture_ref = picture_ref  # Reference to picture
+
+    def to_dict(self):
+        return self.__dict__
+
+    def to_json(self):
+        return json.dumps(self.__dict__)
