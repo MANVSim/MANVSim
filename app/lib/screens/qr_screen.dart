45a227dc20aca300388c16e0451d681b6ab27915
Lukas Voigt <lukasvoigt99@icloud.com>
2024-05-13 12:48:06 +0200

    feat(app) #28: add qr code scanner

---
+++ app/lib/screens/qr_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class QRScreen extends StatefulWidget {
+  const QRScreen({super.key});
+
+  @override
+  _QRScreenState createState() => _QRScreenState();
+}
+
+class _QRScreenState extends State<QRScreen> {
+  bool _isBarcodeDetected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scan QR Code'),
+      ),
+      body: MobileScanner(
+        onDetect: (barcode) {
+          if (!_isBarcodeDetected && barcode.barcodes.isNotEmpty) {
+            setState(() {
+              _isBarcodeDetected = true;
+            });
+            final String code = barcode.barcodes.first.rawValue ?? 'Unknown';
+            Navigator.pop(context, code);
+          }
+        },
+      ),
+    );
+  }
+}
