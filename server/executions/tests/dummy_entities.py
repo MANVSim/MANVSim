a8a9c6a3c0babd75017d4b439184431a6ed0f0f5
Simon Ohlsen <stu216738@mail.uni-kiel.de>
2024-05-13 16:30:09 +0200

    update entities and diagram, dummy methods for testing

--- server/executions/tests/dummy_entities.py
+++ server/executions/tests/dummy_entities.py
@@ -5,14 +5,14 @@ from executions.entities.patient import Patient
 from executions.entities.performed_action import PerformedAction
 from executions.entities.player import Player
 from executions.entities.resource import Resource
-from executions.entities.resource_type import ResourceType
 from executions.entities.scenario import Scenario
 
 
 # -- Create Empty Test Objects --
 
-def create_dummy_scenario(id: int = 0, name: str = "Dummy Scenario", patients: list[Patient] = None):
-    return Scenario(id=id, name=name, patients=patients)
+def create_dummy_scenario(id: int = 0, name: str = "Dummy Scenario", locations: dict[int, Location] = None,
+                          patients: list[Patient] = None):
+    return Scenario(id=id, name=name, locations=locations, patients=patients)
 
 
 def create_dummy_execution(id: int = 0, scenario: Scenario = None, starting_time: int = 0, players: list[Player] = None,
@@ -33,9 +33,9 @@ def create_dummy_patient(id: int = 0, name: str = "Dummy Patient", injuries: str
 
 
 def create_dummy_action(id: int = 0, name: str = "Dummy Action", result: str = "None", picture_ref: str = "dummy.png",
-                        duration_sec: int = 0, resource_types_needed: list[ResourceType] = None):
+                        duration_sec: int = 0, resources_needed: list[str] = None):
     return Action(id=id, name=name, result=result, picture_ref=picture_ref, duration_sec=duration_sec,
-                  resource_types_needed=resource_types_needed)
+                  resources_needed=resources_needed)
 
 
 def create_dummy_performed_action(id: int = 0, time: int = 0, execution_id: str = "dummy", action: Action = None,
@@ -45,17 +45,10 @@ def create_dummy_performed_action(id: int = 0, time: int = 0, execution_id: str 
 
 
 def create_dummy_location(id: int = 0, name: str = "Dummy Location", picture_ref: str = "dummy.png",
-                          location: 'Location' = None):
-    return Location(id=id, name=name, picture_ref=picture_ref, location=location)
+                          resources: list[Resource] = None, location: 'Location' = None):
+    return Location(id=id, name=name, picture_ref=picture_ref, resources=resources, location=location)
 
 
-def create_dummy_resource_type(id: int = 0, name: str = "Dummy Resource Type", picture_ref: str = "dummy.png"):
-    return ResourceType(id=id, name=name, picture_ref=picture_ref)
-
-
-def create_dummy_resource(id: int = 0, location: Location = None, resource_type: ResourceType = None,
-                          quantity: int = 0):
-    return Resource(id=id, location=location, resource_type=resource_type, quantity=quantity)
-
-
+def create_dummy_resource(id: int = 0, location: Location = None, quantity: int = 0, picture_ref: str = "dummy.png"):
+    return Resource(id=id, location=location, quantity=quantity, picture_ref=picture_ref)
 
