757f704d8c74b84678f48f6f670ba28a995e33af
Jon <jonstuehrwoldt@gmx.de>
2024-05-19 15:42:24 +0200

    app #34 add models for action and location

---
+++ app/lib/services/action_service.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:manvsim/models/location.dart';
+
+Future<List<Location>> fetchActions() async {
+  String json = demoJson;
+  List<dynamic> jsonList = jsonDecode(json);
+  return jsonList.map((location) => Location.fromJson(location)).toList();
+}
+
+const String demoJson = """
+[
+  {
+    "id": 1,
+    "name": "Red backpack", 
+    "actions": [{
+      "id": 1,
+      "name": "Scissors",
+      "durationInSeconds": 20
+    }],
+    "locations": [{
+      "id": 2,
+      "name": "Medication pack",
+      "actions": [{
+        "id": 2,
+        "name": "Pain killer",
+        "durationInSeconds": 30
+      }],
+      "locations": []
+      }]
+  },
+  {
+    "id": 3,
+    "name": "RTW", 
+    "actions": [{
+      "id": 3,
+      "name": "EKG",
+      "durationInSeconds": 120
+    }],
+    "locations": [{
+      "id": 4,
+      "name": "Medicine cabinet",
+      "actions": [{
+        "id": 4,
+        "name": "Strong pain killer",
+        "durationInSeconds": 50
+      }],
+      "locations": []
+      }]
+  }
+]""";
