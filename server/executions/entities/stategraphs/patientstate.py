ab9a552c5b44bafb436acd1ee0b77fcca8114968
Simon Ohlsen <stu216738@mail.uni-kiel.de>
2024-05-22 10:57:12 +0200

    add utility methods for DB and time

--- server/executions/entities/stategraphs/patientstate.py
+++ server/executions/entities/stategraphs/patientstate.py
@@ -1,7 +1,8 @@
 import json
 import logging
-import time
 import uuid
+
+from utils import time
 
 
 class PatientState:
@@ -32,7 +33,7 @@ class PatientState:
     def start_timer(self):
         """ Initiates a timer for the state object, if a timelimit is set. """
         if self.timelimit != -1:
-            self.start_time = round(time.time())
+            self.start_time = time.current_time_ms()
             return True
         else:
             logging.error("unable to start error, due to missing run time.")
@@ -47,7 +48,7 @@ class PatientState:
 
     def is_state_outdated(self):
         """ Returns a boolean related to the state timeout. """
-        return self.timelimit != -1 and self.timelimit + self.start_time <= round(time.time())
+        return self.timelimit != -1 and self.timelimit + self.start_time <= time.current_time_ms()
 
     def to_dict(self):
         return self.__dict__
