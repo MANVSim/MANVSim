a58f7991d75b26ed4a2ee99f3b91688b2e2ee6fd
7erra <ichbinterra@gmail.com>
2024-05-28 20:52:34 +0200

    Added first router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World!</div>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
