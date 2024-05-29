b0eef49b157104371ffa8b24bbeeff23dc3c58d8
7erra <ichbinterra@gmail.com>
2024-05-29 20:36:15 +0200

    Add link to scenario page

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,13 +6,20 @@ import {
 } from 'react-router-dom'
 import Root from './routes/root'
 import ErrorPage from './error-page'
+import Scenario from './routes/scenario'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <ErrorPage />
-  }
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/scenario",
+        element: <Scenario />
+      }
+    ]
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
