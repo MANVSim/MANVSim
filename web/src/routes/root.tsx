fd73f11d113a363325818f959aa3d6ab9a28878a
7erra <ichbinterra@gmail.com>
2024-05-29 20:27:57 +0200

    Added link to scenarios on homepage

--- web/src/routes/root.tsx
+++ web/src/routes/root.tsx
@@ -1,7 +1,12 @@
 export default function Root() {
   return (
     <>
-      <div>Root element</div>
+      <h1>MANVSim - Simulation eines Massenanfalls von Verletzten</h1>
+      <div>
+        <ul>
+          <li><a href="/scenario">Szenario</a></li>
+        </ul>
+      </div>
     </>
   )
 }
