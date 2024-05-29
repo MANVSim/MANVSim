e21be7751fc0dc6fcfbdb551af325732cc4080bf
7erra <ichbinterra@gmail.com>
2024-05-29 20:36:29 +0200

    Use client side routing

--- web/src/routes/root.tsx
+++ web/src/routes/root.tsx
@@ -1,11 +1,17 @@
+import { Outlet } from "react-router";
+import { Link } from "react-router-dom";
+
 export default function Root() {
   return (
     <>
       <h1>MANVSim - Simulation eines Massenanfalls von Verletzten</h1>
       <div>
         <ul>
-          <li><a href="/scenario">Szenario</a></li>
+          <li><Link to={"/scenario"}>Scenario</Link></li>
         </ul>
+      </div>
+      <div>
+        <Outlet />
       </div>
     </>
   )
