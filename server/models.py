130bbe86df3f9cb64bc4b3fde90a338399460099
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2024-06-03 13:04:34 +0200

    feat: Add first set of database entities

--- server/models.py
+++ server/models.py
@@ -8,6 +8,58 @@ from app import db
 # user = User(name="foo")
 # db.session.add(user)
 # db.session.commit()
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
+
+
+class Scenario(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(128), nullable=False)
+
+
+class Execution(db.Model):
+    tan = db.Column(db.VARCHAR(255), primary_key=True, nullable=False)
+    scenario_id = db.Column(db.ForeignKey(Scenario.id), nullable=False)
+
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.VARCHAR(255), nullable=False)
+    location_id = db.Column(db.Integer, nullable=True)
+
+
+class Player(db.Model):
+    tan = db.Column(db.VARCHAR(6), primary_key=True, nullable=False)
+    execution_id = db.Column(db.ForeignKey(Execution.tan), nullable=False)
+    location_id = db.Column(db.ForeignKey(Location.id), nullable=False)
+
+
+class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    injuries = db.Column(db.JSON(), nullable=False)
+    activity_diagram = db.Column(db.JSON(), nullable=False)
+    location = db.Column(db.ForeignKey(Location.id), nullable=False)
+
+
+class TakesPartIn(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    scenario_id = db.Column(db.ForeignKey(Scenario.id), nullable=False)
+    patient_id = db.Column(db.ForeignKey(Patient.id), nullable=False)
+
+
+class Ressource(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.VARCHAR(255), nullable=False)
+    location = db.Column(db.ForeignKey(Location.id))
+
+
+class Action(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.VARCHAR(255), nullable=False)
+    picture_ref = db.Column(db.VARCHAR(255), nullable=False)
+    results = db.Column(db.JSON(), nullable=False)
+    duration_secs = db.Column(db.Integer, nullable=False)
+
+
+class RessourcesNeeded(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    action_id = db.Column(db.ForeignKey(Action.id), nullable=False)
+    ressource_id = db.Column(db.ForeignKey(Ressource.id), nullable=False)
