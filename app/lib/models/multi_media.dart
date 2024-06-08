ec27b92ffcb2fccf818c41f8e57445605dc9ced3
Jon <jonstuehrwoldt@gmx.de>
2024-06-08 21:53:14 +0200

    app #79 fix bug

--- app/lib/models/multi_media.dart
+++ app/lib/models/multi_media.dart
@@ -9,8 +9,8 @@ class MultiMedia {
     MultiMedia mm = MultiMedia();
     // Easily add new optional properties
     // Allows for keys to miss or having null value
-    if (mm case {"text": String? text}) mm.text = text;
-    if (mm case {"image_ref": String? imageRef}) mm.imageRef = imageRef;
+    if (json case {"text": String? text}) mm.text = text;
+    if (json case {"image_ref": String? imageRef}) mm.imageRef = imageRef;
     return mm;
   }
 }
