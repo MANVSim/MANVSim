fcc59fc6729837ae278942cb855432630ae606ac
7erra <ichbinterra@gmail.com>
2024-06-09 23:50:39 +0200

    Add login manager

--- server/web/setup.py
+++ server/web/setup.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_login import LoginManager
+from models import WebUser
 from .api.register import api
 
 
 def setup(app: Flask):
     app.register_blueprint(api, url_prefix="/api/web")
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(username):
+        return WebUser.get_by_username(username)
