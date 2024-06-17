7907b05e33f9688925b756a2972a53119f40e28d
Lukas Voigt <lukasvoigt99@icloud.com>
2024-06-17 13:32:40 +0200

    feat(app) #174: implement name screen calls

--- app/lib/screens/name_screen.dart
+++ app/lib/screens/name_screen.dart
@@ -1,11 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:manv_api/api.dart';
 
+import '../services/api_service.dart';
 import 'wait_screen.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class NameScreen extends StatelessWidget {
+class NameScreen extends StatefulWidget {
   const NameScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => NameScreenState();
+}
+
+class NameScreenState extends State<NameScreen> {
+
+
+  final TextEditingController _nameController = TextEditingController();
+
+
+  String? _errorMessage;
+  bool _nameInputFailure = false;
+  bool _isLoading = false;
+
+
+
+  InputDecoration _textFieldDecoration(bool hasInputFailure, String text) {
+    return InputDecoration(
+      labelText: text,
+      fillColor: hasInputFailure ? Colors.red.shade50 : null,
+      filled: hasInputFailure,
+      border: hasInputFailure
+          ? const OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.red),
+      )
+          : null,
+    );
+  }
+
+  void _resetErrorMessage() {
+
+    if (_errorMessage != null || _nameInputFailure) {
+      setState(() {
+        _errorMessage = null;
+        _nameInputFailure = false;
+      });
+    }
+  }
+
+  void _handleSetName() async {
+    String name = _nameController.text;
+
+    print(name);
+
+    if (name.isEmpty) {
+      setState(() {
+        _nameInputFailure = true;
+        _errorMessage = AppLocalizations.of(context)!.nameWarningEmptyFields;
+      });
+    } else {
+
+
+      setState(() {
+        _isLoading = true;
+      });
+
+      String? failureMessage;
+
+      try {
+        ApiService apiService = GetIt.instance.get<ApiService>();
+        await apiService.api.playerSetNamePost(PlayerSetNamePostRequest(name: name), apiService.csrfToken);
+      } catch (e) {
+        failureMessage = e.toString();
+      }
+
+      setState(() {
+        _nameInputFailure = false;
+        _errorMessage = failureMessage;
+        _isLoading = false;
+      });
+    }
+
+
+
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -20,20 +102,36 @@ class NameScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextField(
-                decoration: InputDecoration(
-                  labelText: AppLocalizations.of(context)!.nameName,
+              if (_errorMessage != null) // Show error message if it's not null
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: Colors.red.shade100,
+                  child: Row(
+                    children: [
+                      const Icon(Icons.error, color: Colors.red),
+                      const SizedBox(width: 8),
+                      Expanded(
+                        child: Text(
+                          _errorMessage!,
+                          style: const TextStyle(color: Colors.red),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
+              const SizedBox(height: 8),
+              TextField(
+                  controller: _nameController,
+                  decoration: _textFieldDecoration(_nameInputFailure,
+                      AppLocalizations.of(context)!.nameName),
+                  onChanged: (value) => _resetErrorMessage(),
               ),
               const SizedBox(height: 16),
               ElevatedButton.icon(
                 onPressed: () {
-                  Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (context) => const WaitScreen()),
-                        (Route<dynamic> route) =>
-                    false, // Removes all previous routes
-                  );
+
+                  _handleSetName();
+
                 },
                 icon: const Icon(Icons.start),
                 label: Text(AppLocalizations.of(context)!.nameSubmit),
