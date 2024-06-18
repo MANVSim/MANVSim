e30a401fe66405cb000eb7bcf18fbb995907bbbe
7erra <ichbinterra@gmail.com>
2024-06-18 16:36:02 +0200

    Removed unused isType function

--- web/src/utils.tsx
+++ web/src/utils.tsx
@@ -1,14 +1,3 @@
 export function isLoggedIn() {
   return localStorage.getItem("token") !== null
 }
-
-export function isType<T extends object>(
-  obj: unknown,
-  ...requiredProps: (keyof T)[]
-): obj is T {
-  return (
-    obj !== null &&
-    typeof obj === "object" &&
-    requiredProps.every((prop) => (obj as T)[prop] !== undefined)
-  )
-}
