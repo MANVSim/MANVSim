189f671b48f83d576b72eefaee7f956a611c74ab
7erra <ichbinterra@gmail.com>
2024-06-26 12:58:55 +0200

    Documentation for flask utils

--- server/utils/flask.py
+++ server/utils/flask.py
@@ -7,6 +7,7 @@ from flask_api import status
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from werkzeug.exceptions import BadRequest, NotFound
 
+from execution.entities.execution import Execution
 from execution.run import active_executions
 
 
@@ -16,6 +17,18 @@ class RequiredValueSource(StrEnum):
 
 
 def required(arg: str, converter: Callable, source_enum: RequiredValueSource):
+    """
+    Decorator to ensure that the given parameter is part of the request. The following sources can be used:
+    - `~flask.request.form`
+    - `~flask.request.args`
+
+    :param arg: The argument which has to be present in the form or the args
+    :param converter: Converter function that should be used to convert the string value to the desired value. Should throw "ValueError" when conversion fails
+    :param source_enum: Where the given argument should come from
+    :raises ValueError: source_enum is of the wrong type
+    :raises BadRequest: Either the parameter is missing or the given argument could not be converted
+    :return: The return of the decorated function
+    """
     def decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
@@ -45,7 +58,13 @@ def required(arg: str, converter: Callable, source_enum: RequiredValueSource):
     return decorator
 
 
-def admin_only(func):
+def admin_only(func: Callable):
+    """
+    Decorator for a flask endpoint that only allows access to logged in users with the admin role
+
+    :param func: The function to wrap
+    :return: Whatever the decorated function returns
+    """
     @wraps(func)  # https://stackoverflow.com/a/64534085/11370741
     @jwt_required()
     def wrapper(*args, **kwargs):
@@ -57,7 +76,14 @@ def admin_only(func):
     return wrapper
 
 
-def try_get_execution(id: int):
+def try_get_execution(id: int) -> Execution:
+    """
+    Tries to return the execution from the runtime. If the execution does not exist a 404 Error is raised.
+
+    :param id: ID of the execution
+    :return: The execution object
+    :raises NotFound: Error when execution does not exist
+    """
     try:
         execution = active_executions[id]
     except KeyError:
