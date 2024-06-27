42da93bed58f16b9bf20d3bfcf124c83d460e79a
Yannick Illmann <stu216909@mail.uni-kiel.de>
2024-06-27 12:33:26 +0200

    pr change request

--- server/execution/api/notification.py
+++ server/execution/api/notification.py
@@ -7,7 +7,7 @@ api = Blueprint("api-notification", __name__)
 
 @api.get("/notifications")
 @jwt_required()
-def get_notification():
+def get_notifications():
     """
     Endpoint to retrieve notifications using a notification id. The id indicates the next relevant notification.
     Returns all notifications from the id up to the newest, as well as an updated "next_id" value.
