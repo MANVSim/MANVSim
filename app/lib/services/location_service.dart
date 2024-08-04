47182fc0c10d8cf4c6d38207b1cf4aba8db4f767
Lukas Voigt <lukasvoigt99@icloud.com>
2024-08-04 19:27:15 +0200

    feat(app) #212: extend location_service

--- app/lib/services/location_service.dart
+++ app/lib/services/location_service.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:manvsim/models/location.dart';
+import 'package:manvsim/screens/location_screen.dart';
 import 'package:manvsim/services/api_service.dart';
 
 /// Provides methods to manage [Location].
@@ -18,5 +20,13 @@ class LocationService {
     return apiService.api
         .runLocationLeavePost()
         .then((response) => response?.message);
+  }
+
+  static void goToPatientScreen(int locationId, BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => LocationScreen(locationId: locationId)))
+        .whenComplete(() => {}); // TODO leave location
   }
 }
