a9585e9bcf7914a855ed4e4a9de9887302b6b263
Lukas Voigt <lukasvoigt99@icloud.com>
2024-08-17 15:05:55 +0200

    feat(app) #238: implement patient movement

--- app/lib/services/patient_service.dart
+++ app/lib/services/patient_service.dart
@@ -5,6 +5,7 @@ import 'package:manvsim/models/patient.dart';
 import 'package:manvsim/services/api_service.dart';
 import 'package:manvsim/services/location_service.dart';
 
+import '../models/location.dart';
 import '../screens/patient_screen.dart';
 
 /// Provides methods to manage [Patient].
@@ -34,5 +35,14 @@ class PatientService {
             MaterialPageRoute(
                 builder: (context) => PatientScreen(patientId: patientId)))
         .whenComplete(() => LocationService.leaveLocation());
+  }
+
+  static Future<Duration> movePatient(Patient patient, Location moveTo) async {
+    const int waitTimeSeconds = 5;
+    ApiService apiService = GetIt.instance.get<ApiService>();
+    return apiService.api
+        .runActionPerformMovePatientPost(RunActionPerformMovePatientPostRequest(
+            patientId: patient.id, newLocationId: moveTo.id))
+        .then((value) => const Duration(seconds: waitTimeSeconds));
   }
 }
