e4470a3180103a633572bc9f8b5f4980a304f122
Yannick Illmann <stu216909@mail.uni-kiel.de>
2024-08-27 16:33:59 +0200

    new icon cache, improve UX

--- web/src/components/scenario-data.tsx
+++ web/src/components/scenario-data.tsx
@@ -36,7 +36,6 @@ const ScenarioData: React.FC<ScenarioDataProps> = ({
     const emptyBaseData = {
         id: 0,
         name: "",
-        quantity: null
     }
     const [editData, setEditData] = useState<BaseDataStripped>(emptyBaseData)
     const [editDataQuantity, setEditDataQuantity] = useState<number>(0)
@@ -55,7 +54,7 @@ const ScenarioData: React.FC<ScenarioDataProps> = ({
             const newName: string = value.name + `-${index}`
             if (!(newName in dataNames)) {
                 setDataNames([...dataNames, newName])
-                newDatas.push({ id: value.id, name: newName, quantity: null })
+                newDatas.push({ id: value.id, name: newName })
                 setDataCounter(dataCounter + 1)
             }
         }
@@ -89,7 +88,7 @@ const ScenarioData: React.FC<ScenarioDataProps> = ({
         // filter old name and add newName as data type
         setAddDataList(prevAddDataList => {
             const updatedAddList = prevAddDataList.filter(item => item.name !== data.name);
-            return [...updatedAddList, { id: data.id, name: newName, quantity: null }];
+            return [...updatedAddList, { id: data.id, name: newName }];
         });
 
         // filter newName and mark oldname for deletion as data type
@@ -140,8 +139,9 @@ const ScenarioData: React.FC<ScenarioDataProps> = ({
                                 addDataList.map((item) => (
                                     <div className="d-flex flex-row mt-2" key={item.name}>
                                         <div className="d-flex me-2">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-person align-self-center" viewBox="0 0 16 16">
-                                                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-database-add align-self-center" viewBox="0 0 16 16">
+                                                <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0" />
+                                                <path d="M12.096 6.223A5 5 0 0 0 13 5.698V7c0 .289-.213.654-.753 1.007a4.5 4.5 0 0 1 1.753.25V4c0-1.007-.875-1.755-1.904-2.223C11.022 1.289 9.573 1 8 1s-3.022.289-4.096.777C2.875 2.245 2 2.993 2 4v9c0 1.007.875 1.755 1.904 2.223C4.978 15.71 6.427 16 8 16c.536 0 1.058-.034 1.555-.097a4.5 4.5 0 0 1-.813-.927Q8.378 15 8 15c-1.464 0-2.766-.27-3.682-.687C3.356 13.875 3 13.373 3 13v-1.302c.271.202.58.378.904.525C4.978 12.71 6.427 13 8 13h.027a4.6 4.6 0 0 1 0-1H8c-1.464 0-2.766-.27-3.682-.687C3.356 10.875 3 10.373 3 10V8.698c.271.202.58.378.904.525C4.978 9.71 6.427 10 8 10q.393 0 .774-.024a4.5 4.5 0 0 1 1.102-1.132C9.298 8.944 8.666 9 8 9c-1.464 0-2.766-.27-3.682-.687C3.356 7.875 3 7.373 3 7V5.698c.271.202.58.378.904.525C4.978 6.711 6.427 7 8 7s3.022-.289 4.096-.777M3 4c0-.374.356-.875 1.318-1.313C5.234 2.271 6.536 2 8 2s2.766.27 3.682.687C12.644 3.125 13 3.627 13 4c0 .374-.356.875-1.318 1.313C10.766 5.729 9.464 6 8 6s-2.766-.27-3.682-.687C3.356 4.875 3 4.373 3 4" />
                                             </svg>
                                         </div>
                                         <div className="d-flex me-3 fs-5 w-100">
@@ -168,8 +168,8 @@ const ScenarioData: React.FC<ScenarioDataProps> = ({
                                 data.map((item) => (
                                     <div className={`d-flex flex-row mt-2 ${deleteDataList.includes(item) ? "d-none" : ""}`} key={item.name}>
                                         <div className="d-flex me-2">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-person align-self-center" viewBox="0 0 16 16">
-                                                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-database align-self-center" viewBox="0 0 16 16">
+                                                <path d="M4.318 2.687C5.234 2.271 6.536 2 8 2s2.766.27 3.682.687C12.644 3.125 13 3.627 13 4c0 .374-.356.875-1.318 1.313C10.766 5.729 9.464 6 8 6s-2.766-.27-3.682-.687C3.356 4.875 3 4.373 3 4c0-.374.356-.875 1.318-1.313M13 5.698V7c0 .374-.356.875-1.318 1.313C10.766 8.729 9.464 9 8 9s-2.766-.27-3.682-.687C3.356 7.875 3 7.373 3 7V5.698c.271.202.58.378.904.525C4.978 6.711 6.427 7 8 7s3.022-.289 4.096-.777A5 5 0 0 0 13 5.698M14 4c0-1.007-.875-1.755-1.904-2.223C11.022 1.289 9.573 1 8 1s-3.022.289-4.096.777C2.875 2.245 2 2.993 2 4v9c0 1.007.875 1.755 1.904 2.223C4.978 15.71 6.427 16 8 16s3.022-.289 4.096-.777C13.125 14.755 14 14.007 14 13zm-1 4.698V10c0 .374-.356.875-1.318 1.313C10.766 11.729 9.464 12 8 12s-2.766-.27-3.682-.687C3.356 10.875 3 10.373 3 10V8.698c.271.202.58.378.904.525C4.978 9.71 6.427 10 8 10s3.022-.289 4.096-.777A5 5 0 0 0 13 8.698m0 3V13c0 .374-.356.875-1.318 1.313C10.766 14.729 9.464 15 8 15s-2.766-.27-3.682-.687C3.356 13.875 3 13.373 3 13v-1.302c.271.202.58.378.904.525C4.978 12.71 6.427 13 8 13s3.022-.289 4.096-.777c.324-.147.633-.323.904-.525" />
                                             </svg>
                                         </div>
                                         <div className="d-flex me-3 fs-5 w-100">
@@ -190,7 +190,7 @@ const ScenarioData: React.FC<ScenarioDataProps> = ({
                             }
                         </div>
                     ) : (
-                        <div className="mt-5 d-flex justify-content-center border-bottom">
+                        <div className={`mt-5 d-flex justify-content-center ${addDataList.length > 0 ? "d-none" : ""}`}>
                             <p>
                                 <i>Es sind keine Daten gespeichert.</i>
                             </p>
