6af018647ea972b3c774be825d737a38bfc8d4b8
Yannick Illmann <99326069+YIllmann@users.noreply.github.com>
2024-08-27 17:21:18 +0200

    Bug/server/254 put to endpoint location assignment required duplication of locations (#261)
    * bug fix player cant take/put in empty space
    
    * bugfix put location is top-level in inventory
    
    * changed 200 return, update yml
    
    * updated 200 docu for put and take
    
    * fix take-to location
    
    * fix pyright
    
    ---------
    
    Co-authored-by: Yannick Illmann <stu216909@mail.uni-kiel.de>

--- server/execution/api/location.py
+++ server/execution/api/location.py
@@ -6,6 +6,7 @@ from flask_jwt_extended import jwt_required
 from app_config import csrf
 from execution.entities.execution import Execution
 from execution.entities.location import Location
+from execution.entities.player import Player
 from execution.utils import util
 from event_logging.event import Event
 from utils import time
@@ -70,15 +71,10 @@ def get_location_out_of_location(take_location_ids, to_location_ids):
         take_location_id_list: list[int] = ast.literal_eval(take_location_ids)
         to_location_id_list: list[int] = ast.literal_eval(to_location_ids)
 
-        # locate the new location in the players location
-        to_location = get_location_from_index_list(to_location_id_list,
-                                                   execution)
+        if not take_location_id_list:
+            return "Empty id-list provided. Unable to identify location.", 400
 
-        if to_location is None:
-            return ("To-Location not found. Update your current "
-                    "location-access."), 404
-
-        take_location_parent: Location | None= get_location_from_index_list(
+        take_location_parent: Location | None = get_location_from_index_list(
                                 take_location_id_list[:-1], execution)
         if not take_location_parent:
             return ("Take-Location not found. Update your current "
@@ -91,20 +87,27 @@ def get_location_out_of_location(take_location_ids, to_location_ids):
             return ("Take-Location not found. Update your current "
                     "location-access."), 404
 
-        to_location.add_locations({take_location})
-
-        if not player.location:
-            return "Player is not assigned to a toplevel location.", 418
-
-        if to_location.id == player.location.id:
-            # if to_location is the player location -> the take_location should
-            # be placed in the inventory to guarantee a valid leave action.
-            player.accessible_locations.add(take_location)
-        else:
-            # delete only from the location if the player location is different.
-            # In this case the player does not share the resources with any
-            # other player
+        if not to_location_ids:
+            # location is not nested in inventory
             take_location_parent.remove_location_by_id(take_location.id)
+            player.accessible_locations.add(take_location)
+            if player.location:
+                # if a player takes something from the current location without
+                # leaving the location should be still provided at the location
+                player.location.add_locations({take_location})
+
+            return "Location successfully transferred", 200
+
+        to_location = get_location_from_inventory(to_location_id_list,
+                                                  player)
+
+        if not to_location:
+            return ("To-Location not found. Update your current "
+                    "location-access."), 404
+
+        # nest new location into players inventory.
+        to_location.add_locations({take_location})
+        take_location_parent.remove_location_by_id(take_location.id)
 
         Event.location_take_from(execution_id=execution.id,
                                  time=time.current_time_s(),
@@ -112,7 +115,7 @@ def get_location_out_of_location(take_location_ids, to_location_ids):
                                  take_location_ids=take_location_id_list,
                                  to_location_ids=to_location_id_list).log()
 
-        return {"player_location": player.location.to_dict()}
+        return "Location successfully transferred", 200
 
     except KeyError | ValueError:
         return "Missing or invalid request parameter detected.", 400
@@ -145,18 +148,52 @@ def put_location_to_location(put_location_ids, to_location_ids):
         put_location_id_list: list[int] = ast.literal_eval(put_location_ids)
         to_location_id_list: list[int] = ast.literal_eval(to_location_ids)
 
+        if not put_location_id_list or not to_location_id_list:
+            return "Empty id-list provided. Unable to identify location.", 400
+
         to_location = get_location_from_index_list(to_location_id_list,
                                                    execution)
         if not to_location:
             return ("To-Location not found. Update your current "
                     "location-access."), 404
 
-        put_location_parent: Location | None = get_location_from_index_list(
-                                put_location_id_list[:-1], execution)
+        if not player.location:
+            # player has no patient access
+            put_location_parent: Location | None = get_location_from_inventory(
+                put_location_id_list[:-1], player)
+            if len(put_location_id_list) > 1 and not put_location_parent:
+                # required sub-location not found in the inventory
+                return ("Put-location not found in the players inventory.",
+                        400)
+            elif len(put_location_id_list) == 1:
+                # required put-location is top-level in the inventory
+                put_location = player.get_location_from_inventory(
+                    put_location_id_list[0])
+                assert put_location
+                player.accessible_locations.remove(put_location)
+                to_location.add_locations({put_location})
+
+                Event.location_put_to(execution_id=execution.id,
+                                      time=time.current_time_s(),
+                                      player=player.tan,
+                                      put_location_ids=put_location_id_list,
+                                      to_location_ids=to_location_id_list).log()
+                return "Location successfully transferred", 200
+
+        else:
+            # player has patient access -> inventory is located at patients location
+            put_location_parent: Location | None = get_location_from_index_list(
+                                    put_location_id_list[:-1], execution)
+            if len(put_location_id_list) > 1 and not put_location_parent:
+                return ("Put-Location not found. Update your current "
+                        "location-access."), 404
+            elif len(put_location_id_list) == 1:
+                return ("Trying to move a top level location. You ain't that "
+                        "strong"), 418
 
         if not put_location_parent:
-            return ("Put-Location not found. Update your current "
-                    "location-access."), 404
+            # clause for pyright. Not sure when this 'if' would apply...
+            return "Illegal state detected. Please evaluate the case with the developers."
 
         put_location = put_location_parent.get_location_by_id(
             put_location_id_list[len(put_location_id_list)-1])
@@ -165,14 +202,14 @@ def put_location_to_location(put_location_ids, to_location_ids):
             return ("Put-Location not found. Update your current "
                     "location-access."), 404
 
-        if not player.location:
-            return "Player is not assigned to a toplevel location.", 418
-
-        if put_location_parent == player.location.id:
+        if player.location and put_location_parent == player.location.id:
             # if location parent is the players current location -> only the
             # inventory is edited to guarantee a valid leave action.
             player.accessible_locations.remove(put_location)
         else:
+            if player.get_location_from_inventory(put_location.id):
+                # put location is top-level in inventory
+                player.accessible_locations.remove(put_location)
             # otherwise perform default remove and add action
             put_location_parent.remove_location_by_id(put_location.id)
             to_location.add_locations({put_location})
@@ -183,7 +220,7 @@ def put_location_to_location(put_location_ids, to_location_ids):
                               put_location_ids=put_location_id_list,
                               to_location_ids=to_location_id_list).log()
 
-        return {"player_location": player.location.to_dict()}
+        return "Location successfully transferred", 200
 
     except KeyError | ValueError:
         return "Missing or invalid request parameter detected.", 400
@@ -224,6 +261,23 @@ def get_location_from_index_list(id_list: list[int], execution: Execution):
     for i in range(len(id_list)):
         if i == 0:
             target_location = execution.scenario.locations[id_list[i]]
+        elif not target_location:
+            # target location can be None if the first is 'skipped'...
+            return target_location
+        else:
+            target_location = target_location.get_location_by_id(id_list[i])
+
+    return target_location
+
+
+def get_location_from_inventory(id_list: list[int], player: Player):
+    target_location = None
+    if not id_list:
+        return target_location
+
+    for i in range(len(id_list)):
+        if i == 0:
+            target_location = player.get_location_from_inventory(id_list[i])
         elif not target_location:
             # target location can be None if the first is 'skipped'...
             return target_location
--- server/execution/tests/api/test_location.py
+++ server/execution/tests/api/test_location.py
@@ -1,3 +1,6 @@
+import json
+from http import HTTPStatus
+
 from conftest import generate_token
 
 
@@ -8,3 +11,40 @@ def test_player_inventory(client):
     response_data = response.get_json()
     assert response.status_code == 200
     assert len(response_data["accessible_locations"]) > 0
+
+
+def test_put_without_player_location(client):
+    headers = generate_token(client.application)
+    headers["Content-Type"] = "application/json"
+
+    # leave location
+    response = client.post("/api/run/location/leave", headers=headers)
+    assert response.status_code == HTTPStatus.OK
+
+    form = {
+        "put_location_ids": "[2]",
+        "to_location_ids": "[5]"
+    }
+
+    response = client.post("/api/run/location/put-to", headers=headers,
+                           data=json.dumps(form))
+    assert response.status_code == HTTPStatus.OK
+
+
+def ttest_put_toplevel_inventory_location(client):
+    # Disabled, because the test has IDE debugger use only.
+    headers = generate_token(client.application)
+    headers["Content-Type"] = "application/json"
+
+    form = {
+        "put_location_ids": "[1, 2]",
+        "to_location_ids": "[5]"
+    }
+
+    response = client.post("/api/run/location/put-to", headers=headers,
+                           data=json.dumps(form))
+    assert response.status_code == HTTPStatus.OK
+    response_inventory = client.get("/api/run/player/inventory",
+                                    headers=headers)
+    assert response_inventory.status_code == HTTPStatus.OK
+
