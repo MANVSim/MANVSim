a3254e765edcd60bd0d4b7707e8d39c7b435fca4
Lukas Voigt <lukasvoigt99@icloud.com>
2024-09-06 13:41:11 +0200

    app #86: add expansion overview

--- app/lib/widgets/location_overview.dart
+++ app/lib/widgets/location_overview.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:manvsim/models/location.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:manvsim/widgets/muti_media_view.dart';
+import 'package:manvsim/widgets/media_overview_expansion.dart';
 
 class LocationOverview extends StatefulWidget {
   final Location location;
@@ -15,43 +15,14 @@ class LocationOverview extends StatefulWidget {
 }
 
 class _LocationOverviewState extends State<LocationOverview> {
-  bool _detailsVisible = true;
 
   @override
   Widget build(BuildContext context) {
-    return ExpansionTile(
-      title: Padding(
-          padding: const EdgeInsets.all(8),
-          child: SizedBox(
-              width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    AppLocalizations.of(context)!
-                        .patientName(widget.location.name),
-                    style: DefaultTextStyle.of(context)
-                        .style
-                        .apply(fontSizeFactor: 2.0),
-                  ),
-                ],
-              ))),
-      controlAffinity: ListTileControlAffinity.trailing,
-      shape: const Border(),
-      childrenPadding: const EdgeInsets.only(left: 8.0),
-      initiallyExpanded: _detailsVisible,
-      trailing: (_detailsVisible) ? null : const Icon(Icons.info),
-      onExpansionChanged: (expanded) {
-        setState(() {
-          _detailsVisible = expanded;
-        });
-      },
-      children: [
-        Padding(
-            padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-            child: MultiMediaView(multiMediaCollection: widget.location.media)),
-
-      ],
-    );
+    return MediaOverviewExpansion(media: widget.location.media, children: [
+      Text(
+        AppLocalizations.of(context)!.patientName(widget.location.name),
+        style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+      ),
+    ]);
   }
 }
---
+++ app/lib/widgets/media_overview_expansion.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:manvsim/constants/icons.dart' as icons;
+import 'package:manvsim/models/multi_media.dart';
+
+import 'muti_media_view.dart';
+
+class MediaOverviewExpansion extends StatefulWidget {
+  final List<Widget> children;
+  final MultiMediaCollection media;
+
+  const MediaOverviewExpansion(
+      {super.key, required this.children, required this.media});
+
+  @override
+  State<MediaOverviewExpansion> createState() => _MediaOverviewExpansionState();
+}
+
+class _MediaOverviewExpansionState extends State<MediaOverviewExpansion> {
+  bool _detailsVisible = true;
+
+  Widget _title() {
+    return SizedBox(
+        width: double.infinity,
+        child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: widget.children,
+            )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return (widget.media.isEmpty)
+        ? _title()
+        : ExpansionTile(
+            title: _title(),
+            controlAffinity: ListTileControlAffinity.trailing,
+            shape: const Border(),
+            childrenPadding: const EdgeInsets.only(left: 8.0),
+            initiallyExpanded: _detailsVisible,
+            trailing: (_detailsVisible) ? null : const Icon(icons.Icons.info),
+            onExpansionChanged: (expanded) {
+              setState(() {
+                _detailsVisible = expanded;
+              });
+            },
+            children: [
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                  child: MultiMediaView(multiMediaCollection: widget.media)),
+            ],
+          );
+  }
+}
