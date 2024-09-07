f677d2e66e445efead7fd308b0d0e5237e8d7b7f
Lukas Voigt <lukasvoigt99@icloud.com>
2024-09-07 15:11:38 +0200

    app #303: add condition type to model

---
+++ app/lib/models/conditions.dart
@@ -0,0 +1,29 @@
+
+import 'package:manv_api/api.dart';
+import 'package:manvsim/models/multi_media.dart';
+
+class Condition {
+  final String name;
+  final MultiMediaCollection media;
+
+  Condition({required this.name, required this.media});
+
+  factory Condition.fromApi(MapEntry<String, List<MediaReferencesDTOInner>> entry) {
+    return Condition(
+        name: entry.key,
+        media: MultiMediaCollectionExtension.fromApi(entry.value)
+    );
+  }
+
+
+}
+
+typedef Conditions = List<Condition>;
+
+extension ConditionsExtension on Conditions {
+  static Conditions fromApi(Map<String, List<MediaReferencesDTOInner>> conditions) {
+    return conditions.entries
+        .map((entry) => Condition.fromApi(entry))
+        .toList();
+  }
+}
