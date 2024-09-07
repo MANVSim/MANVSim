5753bfe6d36ec787ebe7dcf351272781d40c113d
Lukas Voigt <lukasvoigt99@icloud.com>
2024-09-07 23:23:17 +0200

    app #303: show resource info in action_result_screen

--- app/lib/screens/action_result_screen.dart
+++ app/lib/screens/action_result_screen.dart
@@ -3,6 +3,7 @@ import 'package:manvsim/models/conditions.dart';
 import 'package:manvsim/services/action_service.dart';
 import 'package:manvsim/widgets/action_overview.dart';
 import 'package:manvsim/widgets/api_future_builder.dart';
+import 'package:manvsim/widgets/media_info.dart';
 import 'package:manvsim/widgets/muti_media_view.dart';
 
 import '../models/action_result.dart';
@@ -10,20 +11,20 @@ import '../models/action_result.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../models/patient.dart';
+import '../models/resource.dart';
 
 class ActionResultScreen extends StatefulWidget {
-
   final Patient patient;
   final String performedActionId;
 
-  const ActionResultScreen({super.key, required this.patient, required this.performedActionId});
+  const ActionResultScreen(
+      {super.key, required this.patient, required this.performedActionId});
 
   @override
   State<ActionResultScreen> createState() => _ActionResultScreenState();
 }
 
 class _ActionResultScreenState extends State<ActionResultScreen> {
-
   late Future<ActionResult?> futureActionResult;
 
   @override
@@ -54,21 +55,36 @@ class _ActionResultScreenState extends State<ActionResultScreen> {
               ));
   }
 
-  _buildUsedResources(List<String> resourceNamesNeeded) {
+  _buildUsedResources(List<Resource> resources) {
     return Card(
         child: SizedBox(
             width: double.infinity,
             child: Padding(
                 padding: const EdgeInsets.all(8),
-                child: (resourceNamesNeeded.isEmpty)
+                child: (resources.isEmpty)
                     ? Text(
                         AppLocalizations.of(context)!.actionNoNeededResources)
                     : ListView.builder(
                         shrinkWrap: true,
                         physics: const ClampingScrollPhysics(),
-                        itemCount: resourceNamesNeeded.length,
+                        itemCount: resources.length,
                         itemBuilder: (context, index) {
-                          return Text(resourceNamesNeeded[index]);
+                          Resource currentResource = resources[index];
+                          return Row(
+                            children: [
+                              Expanded(
+                                  child: Text(currentResource.name,
+                                      overflow: TextOverflow.ellipsis)),
+                              Text(currentResource.quantity.toString()),
+                              SizedBox(
+                                  width: 40,
+                                  child: (currentResource.media.isNotEmpty)
+                                      ? MediaInfo(
+                                          title: currentResource.name,
+                                          media: currentResource.media)
+                                      : null),
+                            ],
+                          );
                         },
                       ))));
   }
@@ -84,30 +100,29 @@ class _ActionResultScreenState extends State<ActionResultScreen> {
             width: double.infinity,
             child: SingleChildScrollView(
               physics: const AlwaysScrollableScrollPhysics(),
-              child: ApiFutureBuilder(future: this.futureActionResult, builder: (context, actionResult) {
-
-                return Column(children: [
-                  Card(
-                      child: ActionOverview(
-                          action: actionResult.performedAction.action,
-                          patient: actionResult.patient)),
-                  const Text('Verwendete Ressourcen'),
-                  _buildUsedResources(actionResult.performedAction.action.resourceNamesNeeded),
-                  const Text('Ergebnis(se)'),
-                  ListView.builder(
-                    shrinkWrap: true, // nested scrolling
-                    physics: const ClampingScrollPhysics(),
-                    itemCount: actionResult.conditions.length,
-                    itemBuilder: (context, index) {
-                      return _buildConditionOverview(
-                          actionResult.conditions[index]);
-                    },
-                  ),
-                ]);
-
-
-
-              },),
+              child: ApiFutureBuilder(
+                future: this.futureActionResult,
+                builder: (context, actionResult) {
+                  return Column(children: [
+                    Card(
+                        child: ActionOverview(
+                            action: actionResult.performedAction.action,
+                            patient: actionResult.patient)),
+                    const Text('Verwendete Ressourcen'),
+                    _buildUsedResources(actionResult.performedAction.resources),
+                    const Text('Ergebnis(se)'),
+                    ListView.builder(
+                      shrinkWrap: true, // nested scrolling
+                      physics: const ClampingScrollPhysics(),
+                      itemCount: actionResult.conditions.length,
+                      itemBuilder: (context, index) {
+                        return _buildConditionOverview(
+                            actionResult.conditions[index]);
+                      },
+                    ),
+                  ]);
+                },
+              ),
             )));
   }
 }
