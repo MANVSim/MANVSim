3d740f5bc8c9993d54f4ac809d5324391d591762
Lukas Voigt <lukasvoigt99@icloud.com>
2024-09-18 00:32:54 +0200

    #332 update server config

--- server/scripts/entrypoint.sh
+++ server/scripts/entrypoint.sh
@@ -3,4 +3,9 @@
 pipenv run flask --app main db init
 pipenv run flask --app main db upgrade
 pipenv run python dbsetup.py
-pipenv run gunicorn -w 1 --threads 32 -b 0.0.0.0:5002 --certfile=fullchain.pem --keyfile=privkey.pem 'main:app'
+pipenv run gunicorn \
+    -w 1 \
+    --threads "${SERVER_THREADS:-16}" \
+    -b 0.0.0.0:5002 --certfile=fullchain.pem --keyfile=privkey.pem \
+    'main:app' \
+    --log-level "${SERVER_LOG_LEVEL:-info}"
