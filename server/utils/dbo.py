42ca48dc4e9d85d6bb0312902be1be6e6ba41ab5
Yannick Illmann <stu216909@mail.uni-kiel.de>
2024-09-18 20:52:05 +0200

    ignore type checker

--- server/utils/dbo.py
+++ server/utils/dbo.py
@@ -14,20 +14,20 @@ def add_vehicles_to_execution_to_session(
     highest_role = db.session.execute(role_query).first()
     tan = str(unique())
     player = (models.Player(
-        tan=tan,
-        execution_id=execution_id if execution_id != 0 else None,  # FK
-        location_id=vehicle_id,
-        role_id=highest_role[0],
-        alerted=False)
+        tan=tan, # type: ignore
+        execution_id=execution_id if execution_id != 0 else None,  # type: ignore
+        location_id=vehicle_id,  # type: ignore
+        role_id=highest_role[0],  # type: ignore
+        alerted=False)  # type: ignore
     )
     db.session.add(player)
 
     vehicle = models.PlayersToVehicleInExecution(
-        execution_id=execution_id if execution_id != 0 else None,  # FK
-        scenario_id=scenario_id,
-        location_id=vehicle_id,
-        vehicle_name=vehicle_name,
-        player_tan=tan,
-        travel_time=travel_time
+        execution_id=execution_id if execution_id != 0 else None,  # type: ignore
+        scenario_id=scenario_id,  # type: ignore
+        location_id=vehicle_id,  # type: ignore
+        vehicle_name=vehicle_name,  # type: ignore
+        player_tan=tan,  # type: ignore
+        travel_time=travel_time  # type: ignore
     )
     db.session.add(vehicle)
