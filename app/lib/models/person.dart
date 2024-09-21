3d54306da9599aac9b33c62b1f852d833ca00dfc
Lukas Voigt <lukasvoigt99@icloud.com>
2024-09-21 15:25:34 +0200

    app #407: add to model and location service

---
+++ app/lib/models/person.dart
@@ -0,0 +1,47 @@
+import 'package:manv_api/api.dart';
+
+class PlayerPerson {
+  String name;
+  String role;
+
+  PlayerPerson({required this.name, required this.role});
+
+  factory PlayerPerson.fromApi(RunLocationPersonsGet200ResponsePlayersInner dto) {
+    return PlayerPerson(
+      name: dto.name!,
+      role: dto.role!,
+    );
+  }
+}
+
+class PatientPerson {
+
+  String name;
+  int id;
+
+  PatientPerson({required this.name, required this.id});
+
+  factory PatientPerson.fromApi(RunLocationPersonsGet200ResponsePatientsInner dto) {
+    return PatientPerson(
+      name: dto.name!,
+      id: dto.id!,
+    );
+  }
+}
+
+typedef PlayerPersons = List<PlayerPerson>;
+typedef PatientPersons = List<PatientPerson>;
+
+class Persons {
+  final PlayerPersons players;
+  final PatientPersons patients;
+
+  Persons({required this.players, required this.patients});
+
+  factory Persons.fromApi(RunLocationPersonsGet200Response dto) {
+    return Persons(
+      players: dto.players.map(PlayerPerson.fromApi).toList(),
+      patients: dto.patients.map(PatientPerson.fromApi).toList(),
+    );
+  }
+}
