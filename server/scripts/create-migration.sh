7467a4e38c5511ae4703167e3b5f8e5e5b3a1647
Yannick Illmann <stu216909@mail.uni-kiel.de>
2024-09-27 15:58:59 +0200

    remove unused files, add docu

--- server/scripts/create-migration.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-flask --app main db migrate
--- server/scripts/start-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Note this will fail from in a Windows environment due to resolving failures for .exe files. Use WSL context instead
-if [ "$1" == "--sync" ]; then
-  echo "Synching Pipfile..."
-  pipenv sync
-fi
-
-flask --app main --debug run -p 5002
